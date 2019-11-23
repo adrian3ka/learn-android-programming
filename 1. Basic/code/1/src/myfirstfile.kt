8f1535e1f2591aa9c69c0b2bafd87aca6c1619c2
Adrian Eka <eekkaaadrian@gmail.com>
2019-11-23 23:26:24 +0700

    add more

---
+++ 1. Basic/code/1/src/myfirstfile.kt
@@ -0,0 +1,9 @@
+// Unit stand for Void
+fun main(args: Array<String>): Unit {
+  println("Hello World")
+  print("Hello World")
+  println("Hello Kotlin")
+  println(10 - 9)
+  println(10 / 9)
+  println(10.0 / 9)
+} 
